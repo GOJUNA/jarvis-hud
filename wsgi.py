9958cb088f4e00feff42466f051a136fc8770042
Danilo Pica <bigbababubu250@gmail.com>
2026-08-28 18:12:31 +0200

    Fix wsgi entry point

--- wsgi.py
+++ wsgi.py
@@ -1,5 +1,8 @@
 # wsgi.py - Entry point für gunicorn
 from web.app import socketio
 
+# Flask-App direkt exportieren (für gunicorn)
+app = socketio.app
+
 if __name__ == "__main__":
-    socketio.run(socketio.app, host="0.0.0.0", port=5000)
+    socketio.run(app, host="0.0.0.0", port=5000)
