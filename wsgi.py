dc5e5e747f6519ba702bd5168745886623df7c72
Danilo Pica <bigbababubu250@gmail.com>
2026-08-28 18:16:43 +0200

    Fix wsgi: use socketio.server

--- wsgi.py
+++ wsgi.py
@@ -1,8 +1,8 @@
 # wsgi.py - Entry point für gunicorn
 from web.app import socketio
 
-# Flask-App direkt exportieren (für gunicorn)
-app = socketio.app
+# Flask-App ist socketio.server (bei Flask-SocketIO)
+app = socketio.server
 
 if __name__ == "__main__":
     socketio.run(app, host="0.0.0.0", port=5000)
